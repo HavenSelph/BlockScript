44d4fea392a0aacc84cea85f041b50f76375403d
HavenSelph <havenselph@gmail.com>
2023-04-12 00:19:30 -0500

    Fixed off by one error with spans

--- BlockScript/lexer.py
+++ BlockScript/lexer.py
@@ -14,7 +14,39 @@ class Lexer:
         self.tokens = []
 
     def location(self, offset: int=0) -> Location:
+        """
+        Get the location of the current character, with an offset
+
+        If offset is 0, will return the current location of the lexer, otherwise will find the location of the
+        character at the offset from the current character.
+
+        :param offset: The offset from the current character (negative or positive)
+        :return: The location of the character at the offset
+        """
+        def get_column(offset: int, line: int=self.line, column: int=self.column, index: int=self.index) -> list[int,int]:
+            """
+            offset can be negative or positive, recursively call this function until offset is 0 and return the column and line
+
+            THIS WAS MADE FOR FUN, WILL BE CHANGED IN FUTURE COMMIT
+            """
+            if line < 1:
+                raise Exception("Line number cannot be less than 1, this is a lexer bug.")
+            if offset < 0:
+                if column+offset >= 1:
+                    return [line, column+offset]
+                else:
+                    return get_column(offset+column, line-1, len(self.text[:index].split("\n")), self.text[:index].rfind("\n")-1)
+            elif offset > 0:
+                if column+offset <= len(self.text[:index].split("\n")[-1]):
+                    return [line, column+offset]
+                else:
+                    return get_column(offset-(len(self.text[:index].split("\n")[-1])-column), line+1, 1, index+1)
+        if offset != 0:
+            return Location(self.filename, *get_column(offset, self.index))
         return Location(self.filename, self.line, self.column+offset)
+
+    def cur_span(self) -> Span:
+        return Span(self.location(), self.location())
 
     def advance(self, i: int=1) -> None:
         if self.cur == "\n":
@@ -32,14 +64,12 @@ class Lexer:
         return self.text[self.index + i]
 
     def push_simple(self, kind: TokenKind, size: int=1) -> None:
-        start = self.location()
+        span = self.cur_span()
         self.advance(size)
-        end = self.location(offset=-1)
-        self.tokens.append(Token(kind, None, Span(start, end)))
+        self.tokens.append(Token(kind, None, span))
 
     def push(self, kind: TokenKind, data: int | float | str | None, start: Location, end: Location) -> None:
         self.tokens.append(Token(kind, data, Span(start, end)))
-
 
     def lex(self) -> list[Token]:
         space = False
@@ -59,7 +89,7 @@ class Lexer:
                 case char if char in characters:
                     self.push_simple(characters[char])
                 case _:
-                    raise Exception(f"Unexpected character {self.cur}")
+                    raise SpanError(self.cur_span(), f"Unexpected character '{self.cur}'")
         self.push_simple(TokenKind.EOF)
         return self.tokens
 
@@ -69,7 +99,7 @@ class Lexer:
         while self.cur and (self.cur.isalnum() or self.cur == "_"):
             identifier += self.cur
             self.advance()
-        end = self.location(offset=-1)
+        end = self.location(-1)
         kind = keywords.get(identifier, TokenKind.Identifier)
         self.push(kind, identifier, start, end)
 
@@ -79,13 +109,14 @@ class Lexer:
         while self.cur and (self.cur.isdigit() or self.cur == "." or self.cur == "_"):
             if self.cur == "_":
                 self.advance()
+                continue
             if self.cur == "." and "." in number:
-                raise Exception("Unexpected character '.'")
+                raise SpanError(Span(start, self.location()), "Unexpected '.' in number")
             if self.cur == "." and self.peek(1) == "_":
-                raise Exception("Unexpected character '_'")
+                raise SpanError(Span(start, self.location()), "Unexpected character '_' after '.'")
             number += self.cur
             self.advance()
-        end = self.location(offset=-1)
+        end = self.location(-1)
         if "." in number:
             self.push(TokenKind.Float, float(number), start, end)
         else:
