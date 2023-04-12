a991c1ac2cc62955c4f08be9c2d5af667d485e72
HavenSelph <havenselph@gmail.com>
2023-04-11 23:33:23 -0500

    Moved span and location to file

--- BlockScript/common.py
+++ BlockScript/common.py
@@ -1,25 +1,28 @@
 from __future__ import annotations
-from dataclasses import dataclass
 
 
-@dataclass
 class Location:
     def __init__(self, filename: str, line: int, column: int) -> None:
         self.filename = filename
         self.line = line
         self.column = column
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"{self.filename}:{self.line}:{self.column}"
 
+    def __eq__(self, other: Location) -> bool:
+        return self.filename == other.filename and self.line == other.line and self.column == other.column
 
-@dataclass
+
 class Span:
     def __init__(self, start: Location, end: Location) -> None:
         self.start = start
         self.end = end
+        self.filename = start.filename
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
+        if self.start == self.end:
+            return f"{self.start}"
         return f"{self.start} - {self.end}"
 
     def extend(self, other: Span) -> Span:
