beae88393394e5e1365cf0265515c46597adf30c
HavenSelph <havenselph@gmail.com>
2023-04-12 02:29:19 -0500

    Interpreter errors

--- BlockScript/Errors/interpreter.py
+++ BlockScript/Errors/interpreter.py
@@ -1,5 +1,18 @@
 from .base import SpanError
+from BlockScript.common import Span
 
 
-class VariableNameError(SpanError):
+class InterpreterError(SpanError):
     pass
+
+
+class VariableNameError(InterpreterError):
+    pass
+
+
+class InvalidOperatorError(InterpreterError):
+    def __init__(self, span: Span, msg: str, left: str, op: str, right: str):
+        self.left = left
+        self.op = op
+        self.right = right
+        super().__init__(span, msg)
